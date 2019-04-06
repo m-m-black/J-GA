c7cc1a2477b97b30b957fe62e8be438fbf6283c8
m-m-black <morganblack1988@gmail.com>
2019-04-06 20:57:35 +1100

    Added interactive menu

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,7 @@
 public class Driver {
 
-    static Main main = new Main();
-
     public static void main(String[] args) {
-        main.start();
+        new Main().start();
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main extends Thread {
 
     Population population;
@@ -7,8 +11,36 @@ public class Main extends Thread {
     double crossoverRate;
     double mutationRate;
 
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    String line;
+    boolean quit = false;
+
     @Override
     public void run() {
         // Show menu, initialise parameters from user input, perform runs
+        System.out.println("System running...");
+        try {
+            while (!quit && (line = bufferedReader.readLine()) != null) {
+                String[] tokens = line.split(" ");
+
+                String command = tokens[0];
+
+                switch (command.toUpperCase()) {
+                    case "PRINT":
+                        System.out.println("You typed [print]");
+                        break;
+                    case "RUN":
+                        System.out.println("You typed [run]");
+                        break;
+                    case "QUIT":
+                        quit = true;
+                        break;
+                    default:
+                        System.out.println("Unknown command");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
